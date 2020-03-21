bdff329febed87f9b972c5892068c62abd3990c8
unknown <fbasoeki@googlemail.com>
2020-03-21 17:16:53 +0100

    Check input data for login.

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <p>Hier kannst du dich einloggen</p>
-    <input type="email" v-model="form.email">
-    <input type="password" v-model="form.email">
+    <p>Login</p>
+    User: <input type="user" v-model="input.user" placeholder="Username"/> <br/> <br/>
+    Password: <input type="password" v-model="input.password" placeholder="Password"/> <br/> <br/>
+    <button type="button" v-on:click="login()"<Login<button/> 
   </div>
 </template>
 
@@ -11,17 +12,36 @@ export default {
   name: 'Login',
   data() {
     return {
-    form: {
-        email: '',
-        password: ''
-    } 
+      input: {
+          email: '',
+          password: ''
+      } 
     }
-},
+  },
+  methods: {
+    login() {
+      if(this.input.user != "" && this.input.password != "") {
+        if(this.input.user == this.$parent.account.user && this.input.password == this.$parent.account.password) {
+          this.$emit("authentification", true);
+          this.$router.replace({name: "Home"});
+        } else {
+          console.log("Incorrect username and/or password!");
+        }
+      } else {
+        console.log("Please enter a username and password!");
+      }
+    }
+  }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#login{
+  width: 500px;
+  border: 1px solid;
+}
 h1, h2 {
   font-weight: normal;
 }
