a142154a99c340d744a6c35f32918687f4114c6a
unknown <fbasoeki@googlemail.com>
2020-03-21 20:14:48 +0100

    Versuch: Login-Page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <p>Login</p>
+    <h2>Login</h2>
     User: <input type="user" v-model="input.user" placeholder="Username"/> <br/> <br/>
     Password: <input type="password" v-model="input.password" placeholder="Password"/> <br/> <br/>
-    <button type="button" v-on:click="login()"<Login<button/> 
+    <button type="button" v-on:click="check_login()"/>Login<button/>
+    <button type="button" v-on:click="register()"/>Register<button/>
   </div>
 </template>
 
 <script>
+import router from "../router"
+import axios from "axios"
+
+account => {
+  user = "test",
+  password = "123"
+}
+
 export default {
   name: 'Login',
   data() {
@@ -19,9 +28,9 @@ export default {
     }
   },
   methods: {
-    login() {
+    check_login() {
       if(this.input.user != "" && this.input.password != "") {
-        if(this.input.user == this.$parent.account.user && this.input.password == this.$parent.account.password) {
+        if(this.input.user == account.user && this.input.password == account.password) {
           this.$emit("authentification", true);
           this.$router.replace({name: "Home"});
         } else {
@@ -30,9 +39,35 @@ export default {
       } else {
         console.log("Please enter a username and password!");
       }
+    },
+    login: (e) => {
+      e.preventDefault()
+      let user = "[account.user]"
+      let password = "[account.password]"
+      let login = () => {
+        let data = {
+          user: user,
+          password: password
+        }
+        axios.post("/api/login", data)
+          .then((response) => {
+            console.log("Logged in")
+            router.push("/Home")
+        })
+          .catch((errors) => {
+            console.log("Login failed")
+          })
+      }
+      login()
     }
+  },
+  name: "Register",
+  register() {
+    this.$router.replace({name: "Register"});
   }
 }
+
+
 
 </script>
 
