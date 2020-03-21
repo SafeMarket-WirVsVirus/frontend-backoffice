530a86ef5908ae7af29b63a2ffc3eb75c936b5dd
Philipp Speck <speck.philipp@gmail.com>
2020-03-21 16:04:06 +0100

    Add Verify Form

--- src/pages/Check.vue
+++ src/pages/Check.vue
@@ -1,34 +1,37 @@
 <template>
   <div>
-    <p>Check Ticket</p>
+    <div>
+        <input type="text" v-model="ticketNumber"><button v-on:click="verify">Verify</button>
+    </div>
+    <div>    
+        <button v-on:click="scanQRCode">Scan QR Code</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Check',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'Check',
+    data () {
+        return {
+            ticketNumber: ''
+        }
+    },
+    methods: {
+        verify: function() {
+            alert(this.ticketNumber);
+        },
+        scanQRCode: function() {
+
+        }
     }
-  }
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    div{
+        margin: 10px 0;
+    }
 </style>
