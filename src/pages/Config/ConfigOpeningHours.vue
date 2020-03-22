c337e388dbf1ff3f1251dffeca57f497d40c0f48
jstask <aaronkessler@Aarons-MBP.fritz.box>
2020-03-22 17:19:42 +0100

    mo-so

--- src/pages/Config/ConfigOpeningHours.vue
+++ src/pages/Config/ConfigOpeningHours.vue
@@ -4,9 +4,9 @@
 
     <div>
       <div role="tablist">
-        <b-card no-body class="mb-1">
+        <b-card no-body class="mb-1" v-for="(day, index) in days" :key="index">
           <b-card-header header-tag="header" class="p-1" role="tab">
-            <b-button block href="#" v-b-toggle.accordion-1 variant="info">Montag</b-button>
+            <b-button block href="#" v-b-toggle.accordion-1 variant="info">{{day}}</b-button>
           </b-card-header>
           <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
             <b-card-body>
@@ -28,18 +28,6 @@
             </b-card-body>
           </b-collapse>
         </b-card>
-
-        <b-card no-body class="mb-1">
-          <b-card-header header-tag="header" class="p-1" role="tab">
-            <b-button block href="#" v-b-toggle.accordion-2 variant="info">Dienstag</b-button>
-          </b-card-header>
-          <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
-            <b-card-body>
-              <b-card-text>Öffnungszeiten</b-card-text>
-            </b-card-body>
-          </b-collapse>
-        </b-card>
-  
       </div>
     </div>
 
@@ -55,6 +43,22 @@ import AbstractStep from "./AbstractStep";
 
 export default {
   name: "ConfigOpeningHours",
-  extends: AbstractStep
+  extends: AbstractStep,
+
+  data() {
+    const days = [
+      "Montag",
+      "Dienstag",
+      "Mittwoch",
+      "Donnerstag",
+      "Freitag",
+      "Samstag",
+      "Sonntag"
+    ];
+    let count: 0;
+    return {
+      days count
+    };
+  }
 };
 </script>
