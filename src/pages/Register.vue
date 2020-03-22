e090fb5ed4d5795207662cb51607d1e86e11bcca
Philipp Speck <speck.philipp@gmail.com>
2020-03-22 19:21:00 +0100

    Registration Form send to API

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -5,7 +5,7 @@
     <form @submit.prevent="handleSubmit">
       <div class="form-group">
         <input
-          placeholder="Vorname*"
+          placeholder="Name"
           class="form-control"
           id="name-input"
           type="text"
@@ -13,11 +13,8 @@
         />
       </div>
       <div class="form-group">
-        <input placeholder="Name" class="form-control" id="1st-name-input" type="text" />
-      </div>
-      <div class="form-group">
         <input
-          placeholder="Passwort*"
+          placeholder="Passwort"
           class="form-control"
           id="password-input"
           type="password"
@@ -26,39 +23,20 @@
       </div>
       <div class="form-group">
         <input
-          placeholder="Passwort wiederholen*"
-          class="form-control"
-          id="password-repeat-input"
-          type="password"
-        />
-      </div>
-      <div class="form-group">
-        <input
-          placeholder="Firma"
-          class="form-control"
-          id="company-input"
-          type="text"
-        />
-      </div>
-      <div class="form-group">
-        <input
-          placeholder="E-Mail*"
+          placeholder="E-Mail"
           class="form-control"
           id="email-input"
           type="email"
           v-model="user.Email"
         />
       </div>
-      <div class="form-group">
-        <input
-          placeholder="Telefon"
-          class="form-control"
-          id="phone-input"
-          type="tel"
-        />
-      </div>
-      *Plichteingabe<br />
-      <button type="submit" class="btn btn-primary">Profil anlegen</button>
+      <button type="submit" class="btn btn-primary" :disabled="isLoading">
+        <span v-if="!isLoading">Profil anlegen</span>
+        <span v-if="isLoading">
+          <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+        </span>
+      </button>
+      <span class="error" v-if="isError">Es ist ein Fehler aufgetreten.</span>
     </form>
 
     <div></div>
@@ -66,8 +44,7 @@
 </template>
 
 <script>
-import axios from 'axios'
-import { api } from '@/config'
+import { HTTP } from '../http'
 
 export default {
   name: "Register",
@@ -78,30 +55,34 @@ export default {
         Email: '',
         Password: '',
         Type: 0
-      }
+      },
+      isLoading: false,
+      isError: false
     }
   },
   methods: {
     handleSubmit() {
-      // alert(
-      //   `Register '${this.user.name}' (${this.user.email}) with ` +
-      //     `password ${this.user.password}`
-      // )
-
-      axios.post(
-        api.user,
-        this.user,
-        {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }
-      ).then(response => {
-        console.log({ response })
-      }).catch((error) => {
-        console.log(error)
+      this.isError = false;
+      this.isLoading = true;
+      HTTP.post('/api/User', this.user)
+      .then(response => {
+        console.log(response);
+        this.isError = false;
+        this.$router.replace({name: "Login"});
       })
+      .catch(error => {
+        console.log(error);
+        this.isLoading = false;
+        this.isError = true;
+      })
+
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.error{
+  padding-left: 10px;
+}
+</style>
