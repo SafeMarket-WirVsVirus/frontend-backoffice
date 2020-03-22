be771cf7bb000eb2aa22b8d169179088af865234
Philipp Speck <speck.philipp@gmail.com>
2020-03-22 16:28:29 +0100

    Adjust Footer Menu according to Screens

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Stores from '@/pages/Stores'
 import Login from '@/pages/Login'
 import Register from '@/pages/Register'
+import Profile from '@/pages/Profile'
 import Check from '@/pages/Check'
 import Config from '@/pages/Config/index'
 import { steps } from '@/config'
@@ -30,6 +31,13 @@ export default new Router({
       name: 'Register',
       component: Register,
       navTitle: 'Register'
+    },
+    {
+      path: '/profile/',
+      name: 'Profile',
+      component: Profile,
+      navTitle: 'Profil',
+      icon: '<svg class="bi bi-person-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"/></svg>'
     },
     {
       path: '/check/:id',
