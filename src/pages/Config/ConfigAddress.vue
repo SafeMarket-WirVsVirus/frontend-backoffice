bd46835250b42ba036005a0eeccaec5a3c563009
jstask <aaronkessler@Aarons-MBP.fritz.box>
2020-03-22 20:43:35 +0100

    use geo

--- src/pages/Config/ConfigAddress.vue
+++ src/pages/Config/ConfigAddress.vue
@@ -1,19 +1,24 @@
 <template>
-
   <div>
+
     <div class="form-group">
       <select id="type" class="form-control" v-model="locationType">
         <option v-for="(type, index) in types"
             :key="index" :value="type" >{{type}}</option>
       </select>
     </div>
+
     <div class="form-group">
-      <input type="number" class="form-control" id="radius" placeholder="Radius in km" v-model="radius" />
+      <input
+        type="number"
+        class="form-control"
+        id="radius"
+        placeholder="Radius in m"
+      />
     </div>
-    <div class="form-group">
-      <button type="button" class="btn btn-primary" @click="checkGeoLocation">Standorte auflisten</button>
-    </div>
-<!--     <div>Adresse</div>
+
+
+     <!--    <div>Adresse</div>
     <div class="form-group">
       <input
         type="text"
@@ -22,13 +27,15 @@
         placeholder="Filiale"
         v-model="companyName"
       />
-    </div>
+    </div> -->
+
     <div class="form-group">
       <select class="form-control" id="googleplace" v-model="placeId">
         <option v-for="(place, index) in locations" :key="index" :value="place.placeId" >{{place.name}} - {{place.address}}
         </option>
       </select>
     </div>
+<!--     </div>
       <input
         type="text"
         class="form-control"
@@ -36,9 +43,9 @@
         placeholder="Straße / Hausnummer"
         v-model="street"
       />
-    </div> -->
-
-<!--     <div class="form-group">
+    </div>
+ -->
+<!--         <div class="form-group">
       <input type="number" class="form-control" id="zip" placeholder="PLZ" v-model="zip" />
     </div> -->
     <div class="form-group">
@@ -52,77 +59,83 @@
 </template>
 
 <script>
-import AbstractStep from './AbstractStep'
-import {HTTP} from '../../http';
+import AbstractStep from "./AbstractStep";
+import { HTTP } from "../../http";
 export default {
-  name: 'ConfigAddress',
+  name: "ConfigAddress",
   extends: AbstractStep,
-  data(){
-    return{
+  data() {
+    return {
+      locations: {},
       types:["bakery","book_store","clothing_store","convenience_store","department_store","drugstore","electronics_store","furniture_store","grocery_or_supermarket","hardware_store","home_goods_store","laundry","liquor_store","pet_store","pharmacy","shoe_store","shopping_mall","store","supermarket"]
-    }
+    };
   },
-  created(){
-    HTTP.get('/api/Location/Search',{
-      params: {
-        type: "supermarket",
-        longitude: 7,
-        latitude: 48,
-        radiusInMeters: 50000
-      }
-    })
+  created() {
+    this.$getLocation()
+      .then(coordinates => {
+        console.log(coordinates)
+        return HTTP.get("/api/Location/Search", {
+          params: {
+            type: "supermarket",
+            longitude: coordinates.lng,
+            latitude: coordinates.lat,
+            radiusInMeters: 50000
+          }
+        });
+      })
       .then(response => {
-        console.log(response.data)
-        this.locations = response.data.locations
+        console.log(response.data);
+        this.locations = response.data.locations;
       })
       .catch(e => {
-        console.log(e)
-      })
-
-  methods: {
-    checkGeoLocation(){
-      this.$getLocation()
-      .then(coordinates => {
-        console.log(coordinates);
-      return coordinates;
+        console.log(e);
       });
-      }
   },
   computed: {
     companyName: {
-      get () {
-        return this.getStoreAttributeByName('name', this.activeStoreIndex) || ''
+      get() {
+        return (
+          this.getStoreAttributeByName("name", this.activeStoreIndex) || ""
+        );
       },
-      set (value) {
-        this.$store.dispatch('stores/setStoreAttribute', {
+      set(value) {
+        this.$store.dispatch("stores/setStoreAttribute", {
           activeStoreIndex: this.activeStoreIndex,
-          name: 'name',
+          name: "name",
           value: value
-        })
+        });
       }
     },
     street: {
-      get () {
-        return this.getStoreAttributeByName('address.street', this.activeStoreIndex) || ''
+      get() {
+        return (
+          this.getStoreAttributeByName(
+            "address.street",
+            this.activeStoreIndex
+          ) || ""
+        );
       },
-      set (value) {
-        this.$store.dispatch('stores/setStoreAttribute', {
+      set(value) {
+        this.$store.dispatch("stores/setStoreAttribute", {
           activeStoreIndex: this.activeStoreIndex,
-          name: 'address.street',
+          name: "address.street",
           value: value
-        })
+        });
       }
     },
     zip: {
-      get () {
-        return this.getStoreAttributeByName('address.zip', this.activeStoreIndex) || ''
+      get() {
+        return (
+          this.getStoreAttributeByName("address.zip", this.activeStoreIndex) ||
+          ""
+        );
       },
-      set (value) {
-        this.$store.dispatch('stores/setStoreAttribute', {
+      set(value) {
+        this.$store.dispatch("stores/setStoreAttribute", {
           activeStoreIndex: this.activeStoreIndex,
-          name: 'address.zip',
+          name: "address.zip",
           value: value
-        })
+        });
       }
     },
     locationType: {
@@ -138,17 +151,19 @@ export default {
       }
     },
     placeId: {
-      get () {
-        return this.getStoreAttributeByName('placeId', this.activeStoreIndex) || ''
+      get() {
+        return (
+          this.getStoreAttributeByName("placeId", this.activeStoreIndex) || ""
+        );
       },
-      set (value) {
-        this.$store.dispatch('stores/setStoreAttribute', {
+      set(value) {
+        this.$store.dispatch("stores/setStoreAttribute", {
           activeStoreIndex: this.activeStoreIndex,
-          name: 'placeId',
+          name: "placeId",
           value: value
-        })
+        });
       }
-    },
+    }
   }
-}
+};
 </script>
