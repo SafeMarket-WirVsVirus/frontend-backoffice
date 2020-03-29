c2ff509c4062b4a3acf8309eb91fbc9e111d4bd9
Philipp Speck <speck.philipp@gmail.com>
2020-03-29 17:30:15 +0200

    Solved Merge Conflicts

--- src/pages/Config/ConfigAddress.vue
+++ src/pages/Config/ConfigAddress.vue
@@ -1,28 +1,6 @@
 <template>
   <div>
-<<<<<<< HEAD
     <location-search></location-search>
-=======
-    <div class="form-group">
-      <select id="type" class="form-control" v-model="locationType" @change="onChange($event)">
-        <option v-for="(type, index) in types" :key="index" :value="type">{{type}}</option>
-      </select>
-    </div>
-
-    <div class="form-group">
-      <input type="number" class="form-control" id="radius" placeholder="Radius in m" v-model="radius" />
-    </div>
-
-    <div class="form-group">
-      <select class="form-control" id="googleplace" v-model="place" @change="placeChange($event)">
-        <option
-          v-for="(place, index) in locations"
-          :key="index"
-          :value="{id:place.placeId, name:place.name, address:place.address}"
-        >{{place.name}} - {{place.address}}</option>
-      </select>
-    </div>
->>>>>>> 17a959d6df10d4546451a08a75ff558eb571719f
 
     <div class="form-group">
       <button type="button" class="btn btn-secondary" @click="backHome">Zurück</button>
@@ -51,35 +29,6 @@ export default {
     };
   },
   methods:{
-<<<<<<< HEAD
-
-=======
-    onChange(){
-      const radiusInt = parseInt(this.radius, 10)
-      const radiusInMeters = radiusInt != null && !isNaN(radiusInt) ? radiusInt : 50000
-      this.$getLocation()
-      .then(coordinates => {
-       return HTTP.get('/api/Location/Search',{
-        params: {
-          type: this.locationType,
-            longitude: coordinates.lng,
-            latitude: coordinates.lat,
-          radiusInMeters
-        }
-      })})
-      .then(response => {
-        console.log(response.data);
-        this.locations = response.data.locations;
-      })
-      .catch(e => {
-        console.log(e)
-      })
-    },
-    placeChange(event){
-      console.log(event.target.value.id)
-      console.log(event.target.value.name)
-    }
->>>>>>> 17a959d6df10d4546451a08a75ff558eb571719f
   },
   computed: {
     locationType: {
